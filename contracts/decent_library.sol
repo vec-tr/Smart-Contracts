5c1cf2d192abf25dedbaaf3f39eb3dc02b3b4168
vhariharmath <vinayak.hariharmath@gmail.com>
2023-05-15 11:25:09 +0530

    library

--- contracts/decent_library.sol
+++ contracts/decent_library.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.0;
 
 contract DecentLibrary {
     address public librarian;
+
     struct BookShelfDetail {
         string title;
-        address holderName;
+        address holder;
         bool reserved;
+        bool available;
+        uint holdingTime;
     }
 
     mapping (string => BookShelfDetail) BookShelfDetails;
@@ -21,17 +24,44 @@ contract DecentLibrary {
         _;
     }
 
-    function ReserveBook(string memory BookTitle) public Librarian () {
+    function HoldBook(string memory BookTitle) public Librarian () {
         if (BookShelfDetails[BookTitle].reserved == true) {
             emit bookAvailability(false);
-            revert("Book Not Available");
+            revert("Book is already reserved");
         }
-        BookShelfDetails[BookTitle].holderName = msg.sender;
+
+
+        if (BookShelfDetails[BookTitle].available == false) {
+            emit bookAvailability(false);
+            revert("Book is not available");
+        }
+
+        BookShelfDetails[BookTitle].holder = msg.sender;
         BookShelfDetails[BookTitle].reserved = true;
+        BookShelfDetails[BookTitle].holdingTime = block.timestamp;
+    }
+
+    function UnHoldBook(string memory BookTitle) public Librarian () {
+            BookShelfDetails[BookTitle].holder = address(0);
+            BookShelfDetails[BookTitle].reserved = false;
+            BookShelfDetails[BookTitle].holdingTime = 0;
+    }
+
+    function AddBookToShelf(string memory BookTitle) public Librarian () {
+            BookShelfDetails[BookTitle].available = true;
+            BookShelfDetails[BookTitle].reserved = false;
     }
 
     function CheckBookAvailaility(string memory BookTitle) public view returns (string memory){
-        require(BookShelfDetails[BookTitle].reserved == true, "Book Not Available");
-        return "Book Available";
+        require(BookShelfDetails[BookTitle].available == true, "Book is not available in the library");
+        require(BookShelfDetails[BookTitle].reserved == false, "Book is already reserved");
+        return "Book is Available to Hold";
+    }
+
+    function TransferBook(string memory BookTitle, address _newHolder) public Librarian () {
+            require(BookShelfDetails[BookTitle].available == true, "Book is not available in the library");
+            BookShelfDetails[BookTitle].holder = _newHolder;
+            BookShelfDetails[BookTitle].reserved = true;
+            BookShelfDetails[BookTitle].holdingTime = block.timestamp;
     }
 }
